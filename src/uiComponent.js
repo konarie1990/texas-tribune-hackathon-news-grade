243123f0b3dc4d58aacd66afd0c2618426486ce9
Ronald Hajewski <ronald.hajewski@gmail.com>
2019-04-11 15:30:51 -0500

    work in progress - still need to connect handleChange, handleSubmit, componentDidMount, etc...

--- src/uiComponent.js
+++ src/uiComponent.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import {
   Button,
   Form,
@@ -9,55 +9,86 @@ import {
   Divider
 } from "semantic-ui-react";
 
-const LoginForm = () => (
-  <div className="login-form">
-    {/*
-      Heads up! The styles below are necessary for the correct render of this example.
-      You can do same with CSS, the main idea is that all the elements up to the `Grid`
-      below must have a height of 100%.
-    */}
-    <style>
-      {`
+// Pass getRefs into onChange, mount component, handle submit and return data
+const getRefs = url => {
+  let base =
+    "https://www.googleapis.com/customsearch/v1?key=AIzaSyB0zqGyHNgmUiWdm9CiofUIldNjhr2jhfM&cx=012673784411427794478:licl7tpt11a&q=";
+  fetch(base + "intext: " + url).then(res => {
+    res.json().then(data => {
+      let items = data["items"].map(r => r.link);
+      let total = data["queries"]["request"][0]["totalResults"];
+      document.getElementById("output").innerHTML = total.toString();
+      console.log("Total is " + total);
+      console.log(items);
+    });
+  });
+};
+
+class LoginForm extends Component {
+  state = { name: "", submittedName: "" };
+
+  handleChange = (e, { name, value }) => this.setState({ [name]: value });
+
+  handleSubmit = () => {
+    const { name } = this.state;
+
+    this.setState({ submittedName: name });
+  };
+
+  render() {
+    const { name, submittedName } = this.state;
+
+    return (
+      <div className="login-form">
+        <style>
+          {`
       body > div,
       body > div > div,
       body > div > div > div.login-form {
         height: 100%;
       }
     `}
-    </style>
-    <Grid textAlign="center" style={{ height: "100%" }} verticalAlign="middle">
-      <Grid.Column style={{ maxWidth: 450 }}>
-        <Header as="h2" color="teal" textAlign="center">
-          News Content Integrity Grade
-        </Header>
-        <Form size="large">
-          <Segment stacked>
-            <Form.Input
-              fluid
-              icon="globe"
-              iconPosition="left"
-              placeholder="Enter URL"
-            />
+        </style>
+        <Grid
+          textAlign="center"
+          style={{ height: "100%" }}
+          verticalAlign="middle"
+        >
+          <Grid.Column style={{ maxWidth: 450 }}>
+            <Header as="h2" color="teal" textAlign="center">
+              News Content Integrity Grade
+            </Header>
+            <Form onSubmit={this.handleSubmit} size="large">
+              <Segment stacked>
+                <Form.Input
+                  onChange={this.handleChange}
+                  name="name"
+                  value={name}
+                  fluid
+                  icon="globe"
+                  iconPosition="left"
+                  placeholder="Enter URL"
+                />
 
-            <Button color="teal" fluid size="large">
-              Search
-            </Button>
-          </Segment>
-        </Form>
-        <Message>
-          <Header as="h1" color="teal">
-            Grade: A
-          </Header>
-          <Divider />
-          <ul>
-            <li href="#">https://www.texastribune.org/2018/03/21</li>
-            <li href="#">https://www.texastribune.org/2018/03</li>
-            <li href="#">https://www.texastribune.org/2018/03/21/judges-ru</li>
-          </ul>
-        </Message>
-      </Grid.Column>
-    </Grid>
-  </div>
-);
-
+                <Button color="teal" fluid size="large">
+                  Search
+                </Button>
+              </Segment>
+            </Form>
+            <Message>
+              <Header as="h1" color="teal">
+                Grade: A
+              </Header>
+              <Divider />
+              <strong>onChange:</strong>
+              <pre>{JSON.stringify({ name }, null, 2)}</pre>
+              <strong>onSubmit:</strong>
+              <pre>{JSON.stringify({ submittedName }, null, 2)}</pre>
+            </Message>
+          </Grid.Column>
+        </Grid>
+      </div>
+    );
+  }
+}
 export default LoginForm;
