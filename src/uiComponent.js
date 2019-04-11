c06c1c54cf11eacde003b1f3710e5c99557dcdd8
Ronald Hajewski <ronald.hajewski@gmail.com>
2019-04-11 14:46:51 -0500

    gui mockup

---
+++ src/uiComponent.js
@@ -0,0 +1,63 @@
+import React from "react";
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Message,
+  Segment,
+  Divider
+} from "semantic-ui-react";
+
+const LoginForm = () => (
+  <div className="login-form">
+    {/*
+      Heads up! The styles below are necessary for the correct render of this example.
+      You can do same with CSS, the main idea is that all the elements up to the `Grid`
+      below must have a height of 100%.
+    */}
+    <style>
+      {`
+      body > div,
+      body > div > div,
+      body > div > div > div.login-form {
+        height: 100%;
+      }
+    `}
+    </style>
+    <Grid textAlign="center" style={{ height: "100%" }} verticalAlign="middle">
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as="h2" color="teal" textAlign="center">
+          News Content Integrity Grade
+        </Header>
+        <Form size="large">
+          <Segment stacked>
+            <Form.Input
+              fluid
+              icon="globe"
+              iconPosition="left"
+              placeholder="Enter URL"
+            />
+
+            <Button color="teal" fluid size="large">
+              Search
+            </Button>
+          </Segment>
+        </Form>
+        <Message>
+          <Header as="h1" color="teal">
+            Grade: A
+          </Header>
+          <Divider />
+          <ul>
+            <li href="#">https://www.texastribune.org/2018/03/21</li>
+            <li href="#">https://www.texastribune.org/2018/03</li>
+            <li href="#">https://www.texastribune.org/2018/03/21/judges-ru</li>
+          </ul>
+        </Message>
+      </Grid.Column>
+    </Grid>
+  </div>
+);
+
+export default LoginForm;
